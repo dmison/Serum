1a0faeef9661e5afebfa2db924c9e41df69a2258
Darrin Mison <tabby@Sekhmet.local>
2015-01-02 20:32:54 +1000

    validating options

--- options.js
+++ options.js
@@ -1,9 +1,52 @@
+var validateField = function(field, group) {
+  if (field.val() === '') {
+    group.addClass('has-error');
+  } else {
+    group.removeClass('has-error');
+  }
+}
+
+$(document).ready(function() {
+  restore_options();
+
+  $('#git_user').change(function() {
+    validateField($('#git_user'), $('#git_user_grp'));
+  });
+  $('#git_user').keyup(function() {
+    validateField($('#git_user'), $('#git_user_grp'));
+  });
+
+  $('#git_repo').change(function() {
+    validateField($('#git_repo'), $('#git_repo_grp'));
+  });
+  $('#git_repo').keyup(function() {
+    validateField($('#git_repo'), $('#git_repo_grp'));
+  });
+
+  $('#drafts_dir').change(function() {
+    validateField($('#drafts_dir'), $('#drafts_dir_grp'));
+  });
+  $('#drafts_dir').keyup(function() {
+    validateField($('#drafts_dir'), $('#drafts_dir_grp'));
+  });
+
+  $('#token').change(function() {
+    validateField($('#token'), $('#token_grp'));
+  });
+  $('#token').keyup(function() {
+    validateField($('#token'), $('#token_grp'));
+  });
+
+
+  $('#saveBtn').click(save_options);
+});
+
 // Saves options to chrome.storage
 function save_options() {
-  var git_user = document.getElementById('git_user').value;
-  var git_repo = document.getElementById('git_repo').value;
-  var drafts_dir = document.getElementById('drafts_dir').value;
-  var token = document.getElementById('token').value;
+  var git_user = $('#git_user').val();
+  var git_repo = $('#git_repo').val();
+  var drafts_dir = $('#drafts_dir').val();
+  var token = $('#token').val();
 
   chrome.storage.sync.set({
     git_user: git_user,
@@ -23,20 +66,24 @@ function save_options() {
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
 function restore_options() {
-  // Use default value color = 'red' and likesColor = true.
-  chrome.storage.sync.get(
-    [
-      "git_user",
-      "git_repo",
-      "drafts_dir",
-      "token"
-    ],
-    function(item) {
-      document.getElementById('git_user').value = item.git_user;
-      document.getElementById('git_repo').value = item.git_repo;
-      document.getElementById('drafts_dir').value = item.drafts_dir;
-      document.getElementById('token').value = item.token;
-    });
-}
-document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('save').addEventListener('click', save_options);
+    // Use default value color = 'red' and likesColor = true.
+    chrome.storage.sync.get({
+        git_user: '',
+        git_repo: '',
+        drafts_dir: '_drafts',
+        token: ''
+      },
+      function(item) {
+        document.getElementById('git_user').value = item.git_user;
+        document.getElementById('git_repo').value = item.git_repo;
+        document.getElementById('drafts_dir').value = item.drafts_dir;
+        document.getElementById('token').value = item.token;
+
+        validateField($('#git_user'), $('#git_user_grp'));
+        validateField($('#git_repo'), $('#git_repo_grp'));
+        validateField($('#drafts_dir'), $('#drafts_dir_grp'));
+        validateField($('#token'), $('#token_grp'));
+
+      });
+  }
+  // document.addEventListener('DOMContentLoaded', restore_options);
