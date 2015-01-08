c247268d4fde273eacbcb85910aa29c795de02b3
Darrin Mison <tabby@Sekhmet.local>
2015-01-08 11:49:15 +1000

    show posting progress, success and failure messages

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -17,58 +17,73 @@ var date = today.format('YYYY-MM-DD HH:mm');
 var title = '';
 var quote = '';
 var url = '';
-var filename='';
+var filename = '';
 
-var postArticle = function(){
+var postArticle = function() {
 
   //check config values first, if any are missing then abort and show error message
+
+
+
 
   var content = window.btoa(unescape(encodeURIComponent($('#quote').val())));
   filename = document.getElementById('filename').value;
 
   var base = 'https://api.github.com';
-  var url = base+'/repos/'+gitUser+'/'+gitRepo+'/contents/'+DraftsDir+'/'+filename;
+  var url = base + '/repos/' + gitUser + '/' + gitRepo + '/contents/' + DraftsDir + '/' + filename;
 
   var reqObject = {
-    'message': 'adding draft: '+title,
+    'message': 'adding draft: ' + title,
     'content': content
   };
 
   var reqString = JSON.stringify(reqObject);
 
-  url = url+'?access_token='+token;
+  $('.statusError').hide();
+  $('#progress').show();
 
+  url = url + '?access_token=' + token;
+  $('#progress').show();
   $.ajax({
     type: 'PUT',
     url: url,
     data: reqString,
     dataType: 'json',
     contentType: 'application/json',
-    error: function (jqXHR) {
+    error: function(jqXHR) {
       // display error message
+      $('#progress').hide();
       $('#status').text(jqXHR.statusText);
       $('#code').text(jqXHR.status);
       $('#errorMessage').text(jqXHR.responseJSON.message);
-      $('.status').show();
+      $('.statusError').show();
     },
-    success: function( data, textStatus, jqXHR ){
-      $('.status').show();
-      console.log(JSON.stringify(jqXHR));
+    success: function() {
 
-      // disable text inputs and change post button to close
-      // display success message + 'this window will close in 5 seconds'
-      // set timeout to dismiss pop-up in 5 seconds
+      // disable text inputs
+      $('#quote').attr('disabled', 'disabled');
+      $('#filename').attr('disabled', 'disabled');
+
+      // hide post button and show close
+      $('#postButton').hide();
+      $('#closeButton').show();
+
+      // display success message
+      $('#progress').hide();
+      $('.statusSuccess').show();
+
     }
 
   });
 
 };
 
-$( document ).ready(function() {
-  $('#postbutton').click(postArticle);
-  $('#optionslink').click(function(){
-    window.open(chrome.extension.getURL('options.html'));
+$(document).ready(function() {
+  $('#postButton').click(postArticle);
+  $('#closeButton').click(function() {
+    window.close();
   });
+
 });
 
 
@@ -86,26 +101,26 @@ $( document ).ready(function() {
 */
 
 
-var formatPost = function(){
+var formatPost = function() {
 
-  var body = quote.split('\n').map(function(line){
-    return '> '+line;
+  var body = quote.split('\n').map(function(line) {
+    return '> ' + line;
   }).join('\n');
 
-  body = '---\n'+  'layout: post\n'+'title: '+title+'\n'+ 'date: '+ date+'\n'+  'url: '+url+'\n'+  '---\n'+  '['+title+']('+url+')\n\n'+body;
+  body = '---\n' + 'layout: post\n' + 'title: ' + title + '\n' + 'date: ' + date + '\n' + 'url: ' + url + '\n' + '---\n' + '[' + title + '](' + url + ')\n\n' + body;
 
   return body;
 };
 
 var loadSettings = function(done) {
   chrome.storage.sync.get(['gitRepo', 'gitUser', 'DraftsDir', 'token'],
-  function(item) {
-    gitRepo = item.gitRepo;
-    gitUser = item.gitUser;
-    DraftsDir = item.DraftsDir;
-    token = item.token;
-    done();
-  });
+    function(item) {
+      gitRepo = item.gitRepo;
+      gitUser = item.gitUser;
+      DraftsDir = item.DraftsDir;
+      token = item.token;
+      done();
+    });
 };
 
 // once the popup had fully loaded get the selection from the page and populate the editor box
@@ -127,8 +142,8 @@ document.addEventListener('DOMContentLoaded', function() {
 
         var body = formatPost();
 
-        filename = day+' '+title;
-        filename = filename.replace(/\W+/g, '-')+'.markdown';
+        filename = day + ' ' + title;
+        filename = filename.replace(/\W+/g, '-') + '.markdown';
 
 
         var bodyElement = document.getElementById('quote');
