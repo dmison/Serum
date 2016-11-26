decbe4c48327244de932836f9a2d816ce4da00be
D Mison <dmison@me.com>
2016-11-26 20:22:05 +1000

    added context menu item

--- app/scripts/eventPage.js
+++ app/scripts/eventPage.js
@@ -1,6 +1,20 @@
 /*global sendServiceRequest:false */
 'use strict';
 
+const doPopUp = (tab) => {
+  var maxHeight = window.screen.availHeight-50;
+  var height = maxHeight < 600 ? maxHeight : 600;
+  chrome.windows.create({url:'popup.html', height: height, width: 800, type: 'panel'}, (window)=>{
+    setTimeout(()=>{
+      chrome.tabs.sendMessage(
+        tab.id, {
+          method: 'getSelection'
+        });
+    }, 1500);
+  });
+
+};
+
 chrome.runtime.onInstalled.addListener(function(details) {
   if (details.reason === 'install') {
     chrome.tabs.create({
@@ -16,16 +30,16 @@ chrome.runtime.onInstalled.addListener(function(details) {
 });
 
 
-chrome.browserAction.onClicked.addListener(function(tab) {
-  var maxHeight = window.screen.availHeight-50;
-  var height = maxHeight < 600 ? maxHeight : 600;
-  chrome.windows.create({url:'popup.html', height: height, width: 800, type: 'panel'}, (window)=>{
-    setTimeout(()=>{
-      chrome.tabs.sendMessage(
-        tab.id, {
-          method: 'getSelection'
-        });
-    }, 1500);
-  });
+chrome.browserAction.onClicked.addListener(doPopUp);
 
+chrome.contextMenus.create({
+  id: 'link-serum',
+  title: 'Create post with Serum',
+  contexts: ['all']
+});
+
+chrome.contextMenus.onClicked.addListener((info,tab)=>{
+  if(info.menuItemId === 'link-serum'){
+    doPopUp(tab);
+  }
 });
